5acde5b6961da738c3c9f82c1c139ffcb09b182d
Oleg Marko <olegmarko7@gmail.com>
2018-07-03 23:48:45 +0300

    Added REST api methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const postRouter = require('./routes/post');
 const keys = require('./keys');
 
 const port = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+app.use('/api/post', postRouter);
 app.use(express.static(clientPath));
 
 app.listen(port, () => {
